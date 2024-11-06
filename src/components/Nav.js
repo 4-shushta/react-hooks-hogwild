f0812215cf911bb09135199e588d28ab3ad780d7
4-shushta <knucksslimey@gmail.com>
2024-11-07 00:08:31 +0300

    done challenge

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,17 @@
 import React from "react";
-import piggy from "../assets/porco.png";
+import piggy from "../assets/porco.png"; // Importing the pig image
+import "./App.css"; // Importing the styles
 
 const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">HogWild</span>
-			<div className="TwirlyPig">
-				<img src={piggy} className="App-logo" alt="piggy" />
-			</div>
-			<span className="normalText">
-				A React App for County Fair Hog Fans
-			</span>
-		</div>
-	);
+  return (
+    <div className="navWrapper">
+      <span className="headerText">HogWild</span>
+      <div className="TwirlyPig">
+        <img src={piggy} className="App-logo" alt="piggy" />
+      </div>
+      <span className="normalText">A React App for County Fair Hog Fans</span>
+    </div>
+  );
 };
 
 export default Nav;
